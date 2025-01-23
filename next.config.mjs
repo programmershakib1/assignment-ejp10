5501df3f8fbc8d1b2d17507cc1bffbb3f319f3f2
programmershakib <programmershakibinfo@gmail.com>
2025-01-23 20:30:46 +0600

    error solve

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import NavbarClient from "./NavbarClient";
+import Link from "next/link";
 
 const Navbar = async () => {
   const { getUser } = getKindeServerSession();
+
   const user = await getUser();
 
-  return <NavbarClient user={user} />;
+  return (
+    <div className="flex justify-between items-center">
+      <div>Logo</div>
+      <div className="flex gap-5">
+        <Link href="/">Home</Link>
+        <Link href="/profile">Profile</Link>
+        {user ? (
+          <Link href="/api/auth/logout">Logout</Link>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
