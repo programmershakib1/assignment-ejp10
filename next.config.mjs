5e093d0f9e5776837b620e27f2cffd3d6432b384
programmershakib <programmershakibinfo@gmail.com>
2025-01-23 20:14:15 +0600

    dynamic route fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Recommended for catching errors
-    swcMinify: true,       // Helps optimize the build
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,11 @@
-"use client";
-
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import Link from "next/link";
+import NavbarClient from "./NavbarClient";
 
 const Navbar = async () => {
   const { getUser } = getKindeServerSession();
-
   const user = await getUser();
 
-  return (
-    <div className="flex justify-between items-center">
-      <div>Logo</div>
-      <div className="flex gap-5">
-        <Link href="/">Home</Link>
-        <Link href="/profile">Profile</Link>
-        {user ? (
-          <Link href="/api/auth/logout">Logout</Link>
-        ) : (
-          <Link href="/api/auth/login">Login</Link>
-        )}
-      </div>
-    </div>
-  );
+  return <NavbarClient user={user} />;
 };
 
 export default Navbar;
